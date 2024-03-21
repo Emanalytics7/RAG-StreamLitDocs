9ce3cd64ec265ea39394cbf1b70cddf2414daac1
Emanalytics <emannisar777@gmail.com>
2024-03-21 06:53:51 +0500

    Updated app

--- app.py
+++ app.py
@@ -1,6 +1,46 @@
+from dotenv import load_dotenv
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import faiss
+
+
 def main():
-    print('Hello world')
+    load_dotenv()
+    st.set_page_config(page_title="Ask your PDF")
+    st.header("Ask your PDF 🤔")
+    
+    # upload file
+    pdf = st.file_uploader('Upload your PDF', type='pdf')
+    
+    # extract the text
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+        # split the text into chunks
+        text_splitter = CharacterTextSplitter(
+            separator='\n',
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+        chunks = text_splitter.split_text(text)
+
+        # create embeddings
+        embeddings = OpenAIEmbeddings()
+        knowledge_base = faiss.from_texts(chunks, embeddings)
+
+
+        st.write(chunks)
+        
+
+
 
 
 if __name__ == "__main__":
     main()
+    
