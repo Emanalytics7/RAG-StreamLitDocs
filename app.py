2888f1a426a9c9246131cb6972aefbd50130fba5
Emanalytics <emannisar777@gmail.com>
2024-03-21 17:33:13 +0500

    updated

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import faiss
-
-
+from langchain_community.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain_openai import OpenAI
+from langchain.callbacks import get_openai_callback
 def main():
     load_dotenv()
     st.set_page_config(page_title="Ask your PDF")
@@ -32,13 +33,20 @@ def main():
 
         # create embeddings
         embeddings = OpenAIEmbeddings()
-        knowledge_base = faiss.from_texts(chunks, embeddings)
+        knowledge_base = FAISS.from_documents(chunks, embeddings)
 
+        # show user input
+        user_question = st.text_input('Ask a question about your PDF:')
+        if user_question:
+            docs = knowledge_base.similarity_search(user_question)
 
-        st.write(chunks)
-        
+            llm = OpenAI()      
+            chain = load_qa_chain(llm, chain_type='stuff')
+            with get_openai_callback() as cb:
+               response = chain.run(input_documents=docs, question=user_question)
+               print(cb)
 
-
+            st.write(response)
 
 
 if __name__ == "__main__":
