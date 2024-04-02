38179b741c8e0c7d0a1c144a24a3a2f6526a6190
Emanalytics <emannisar777@gmail.com>
2024-04-02 23:39:14 +0500

    add gitignore

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Dell\Documents\GitHub\RAG-StreamLitDocs\.venv
+set VIRTUAL_ENV=c:\Users\Dell\Documents\GitHub\RAG-StreamLitDocs\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
